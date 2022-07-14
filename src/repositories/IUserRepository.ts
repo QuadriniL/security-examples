c941d4439e323694ea1b2d4d9c6fa8b9a958de1c
Lucas Quadrini <lucasquadrini@MacBook-Pro-de-Lucas.local>
2022-07-14 09:57:33 -0300

    Refator: Modeling design pattern

--- src/repositories/IUserRepository.ts
+++ src/repositories/IUserRepository.ts
@@ -1,4 +1,4 @@
-import {User} from "../Entities/UserEntity";
+import {User} from "../entities/UserEntity";
 
 export interface IUserRepository {
     getUserByName(name: string): Promise<User | undefined>;
